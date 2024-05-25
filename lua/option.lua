92ab5cea83122548a7472afc48111d4b607716de
kfatyuip <kfatyuip@gmail.com>
2024-05-25 21:51:25 +0800

    initialize

---
+++ lua/option.lua
@@ -0,0 +1,12 @@
+local map = vim.keymap.set
+
+vim.opt["tabstop"] = 4
+vim.opt["shiftwidth"] = 4
+
+vim.wo.number = true
+
+local builtin = require('telescope.builtin')
+map('n', '<leader>ff', builtin.find_files, {})
+map('n', '<leader>fg', builtin.live_grep, {})
+map('n', '<leader>fb', builtin.buffers, {})
+map('n', '<leader>fh', builtin.help_tags, {})
