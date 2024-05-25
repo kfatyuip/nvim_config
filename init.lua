e5e2550b68c8d7114c10011a8c51894f79281af4
kfatyuip <kfatyuip@gmail.com>
2024-05-25 22:10:10 +0800

    add lualine

--- init.lua
+++ init.lua
@@ -6,10 +6,11 @@ vim.call('plug#begin')
 Plug('neoclide/coc.nvim', {['branch'] =  'master', ['do'] = 'npm ci'})
 Plug('nvim-lua/plenary.nvim')
 Plug('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.6' })
-Plug('junegunn/fzf.vim')
 Plug('folke/tokyonight.nvim')
 Plug('MunifTanjim/nui.nvim')
-Plug('nvim-neo-tree/neo-tree.nvim', {['branch'] = 'v3.x', ['requires'] = {"nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim"}})
+Plug('nvim-lualine/lualine.nvim')
+Plug('nvim-tree/nvim-web-devicons')
+Plug('nvim-neo-tree/neo-tree.nvim', {['branch'] = 'v3.x', ['requires'] = {'nvim-lua/plenary.nvim', 'MunifTanjim/nui.nvim'}})
 
 vim.call('plug#end')
 
@@ -17,3 +18,7 @@ require('options/cocnvim')
 require('option')
 
 vim.cmd[[colorscheme tokyonight-storm]]
+
+require('lualine').setup {
+  options = { theme  = 'horizon' },
+}
