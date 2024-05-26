9f2a1815f20f370099a3acdbcc57149895bc445a
kfatyuip <kfatyuip@gmail.com>
2024-05-26 18:47:49 +0800

    bufferline keymap

--- lua/option.lua
+++ lua/option.lua
@@ -13,3 +13,7 @@ map('n', '<leader>ff', builtin.find_files, {})
 map('n', '<leader>fg', builtin.live_grep, {})
 map('n', '<leader>fb', builtin.buffers, {})
 map('n', '<leader>fh', builtin.help_tags, {})
+
+local bufferline = require('bufferline')
+map('n', 'gb', ':BufferLinePick<CR>', {})
+map('n', 'gD', ':BufferLinePickClose<CR>', {})
