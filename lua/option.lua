0f3041e7148a937095cb75360d882d9653ebbcae
kfatyuip <kfatyuip@gmail.com>
2024-06-01 19:03:16 +0800

    feat: add telescope jumplist keymap

--- lua/option.lua
+++ lua/option.lua
@@ -14,6 +14,7 @@ map('n', '<leader>ff', builtin.find_files, {})
 map('n', '<leader>fg', builtin.live_grep, {})
 map('n', '<leader>fb', builtin.buffers, {})
 map('n', '<leader>fh', builtin.help_tags, {})
+map('n', '<leader>fj', builtin.jumplist, {})
 
 local bufferline = require('bufferline')
 map('n', 'gb', ':BufferLinePick<CR>', {})
