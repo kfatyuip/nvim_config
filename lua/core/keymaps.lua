1652bbe28ec97073eb8c71a60f785c558650dd11
Kieran Moy <kfatyuip@gmail.com>
2024-07-20 22:52:44 +0800

    add rust_analyzer lsp config

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,17 +3,47 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
-map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
-map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "nvimtree focus window" })
+map(
+    "n",
+    "<C-n>",
+    "<cmd>NvimTreeToggle<CR>",
+    { desc = "nvimtree toggle window" }
+)
+map(
+    "n",
+    "<leader>e",
+    "<cmd>NvimTreeFocus<CR>",
+    { desc = "nvimtree focus window" }
+)
 
 map("n", "<leader>fm", function()
-  require("conform").format { lsp_fallback = true }
+    require("conform").format({ lsp_fallback = true })
 end, { desc = "format files" })
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "telescope find files" })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "telescope live_grep" })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "telescope buffers" })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "telescope help_tags" })
+local builtin = require("telescope.builtin")
+vim.keymap.set(
+    "n",
+    "<leader>ff",
+    builtin.find_files,
+    { desc = "telescope find files" }
+)
+vim.keymap.set(
+    "n",
+    "<leader>fg",
+    builtin.live_grep,
+    { desc = "telescope live_grep" }
+)
+vim.keymap.set(
+    "n",
+    "<leader>fb",
+    builtin.buffers,
+    { desc = "telescope buffers" }
+)
+vim.keymap.set(
+    "n",
+    "<leader>fh",
+    builtin.help_tags,
+    { desc = "telescope help_tags" }
+)
 
 -- map("n", "<leader>ft", require("telescope").extensions.flutter.commands)
