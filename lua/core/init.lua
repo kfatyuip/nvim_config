bf5ed90bfd35c6eb51cf832e9f107b7ec68d252e
Kieran Moy <kfatyuip@gmail.com>
2024-07-20 22:25:51 +0800

    update nvim-tree config

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -36,6 +36,12 @@ require("lazy").setup({
 		config = function()
 			require("nvim-tree").setup({
 				sync_root_with_cwd = true,
+  				sort = {
+				    sorter = "case_sensitive",
+				},
+	  			filters = {
+					dotfiles = true,
+				},
 			})
 		end,
 	},
