50a34a30c86e703e7d7fe3daaa2974e750f89c2f
Kieran Moy <kfatyuip@gmail.com>
2024-07-21 16:25:15 +0800

    refactor: update cmp.lua

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -50,8 +50,8 @@ cmp.setup({
       end
     end, { "i", "s" }),
 
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-d>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.abort(),
     ["<CR>"] = cmp.mapping.confirm({ select = true }),
