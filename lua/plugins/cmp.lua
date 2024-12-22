db92f8bbb443224d9eaf2f4fee823c7282a7df0e
Kieran Moy <kfatyuip@gmail.com>
2024-12-22 15:16:23 +0800

    update cmp.lua

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -4,6 +4,34 @@ local cmp = require("cmp")
 require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip").filetype_extend("dart", { "flutter" })
 
+local cmp_kinds = {
+  Text = "",
+  Method = "󰆧",
+  Function = "󰊕",
+  Constructor = "",
+  Field = "󰇽",
+  Variable = "󰂡",
+  Class = "󰠱",
+  Interface = "",
+  Module = "",
+  Property = "󰜢",
+  Unit = "",
+  Value = "󰎠",
+  Enum = "",
+  Keyword = "󰌋",
+  Snippet = "",
+  Color = "󰏘",
+  File = "󰈙",
+  Reference = "",
+  Folder = "󰉋",
+  EnumMember = "",
+  Constant = "󰏿",
+  Struct = "",
+  Event = "",
+  Operator = "󰆕",
+  TypeParameter = "󰅲",
+}
+
 cmp.setup({
   snippet = {
     expand = function(args)
@@ -32,6 +60,22 @@ cmp.setup({
     ["<C-e>"] = cmp.mapping.abort(),
     ["<CR>"] = cmp.mapping.confirm({ select = true }),
   }),
+  formatting = {
+    format = function(_, vim_item)
+      -- Kind icons
+      vim_item.kind = string.format("%s %s", cmp_kinds[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
+      -- Source
+      -- vim_item.menu = ({
+      --   buffer = "[Buffer]",
+      --   nvim_lsp = "[LSP]",
+      --   luasnip = "[LuaSnip]",
+      --   nvim_lua = "[Lua]",
+      --   latex_symbols = "[LaTeX]",
+      -- })[entry.source.name]
+      return vim_item
+    end,
+  },
+
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
     { name = "buffer" },
@@ -44,6 +88,9 @@ cmp.setup.cmdline({ "/", "?" }, {
   mapping = cmp.mapping.preset.cmdline(),
   sources = {
     { name = "buffer" },
+  },
+  view = {
+    entries = { name = "wildmenu", separator = "|" },
   },
 })
 
