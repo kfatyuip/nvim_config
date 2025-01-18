35cbb1f07e2c2cb7fdaa74f9b23c7fe5d291ab06
Kieran Moy <kfatyuip@gmail.com>
2025-01-18 22:45:48 +0800

    disable nvim-cmp for ':'

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -93,13 +93,3 @@ cmp.setup.cmdline({ "/", "?" }, {
     entries = { name = "wildmenu", separator = "|" },
   },
 })
-
-cmp.setup.cmdline(":", {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = "path" },
-  }, {
-    { name = "cmdline" },
-  }),
-  matching = { disallow_symbol_nonprefix_matching = false },
-})
