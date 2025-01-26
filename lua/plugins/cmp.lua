fa4c8ea9c783742e9eec67dba23de76bf3982cad
Kieran Moy <kfatyuip@gmail.com>
2025-01-26 15:14:51 +0800

    Revert "disable nvim-cmp for ':'"
    This reverts commit 35cbb1f07e2c2cb7fdaa74f9b23c7fe5d291ab06.

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -93,3 +93,13 @@ cmp.setup.cmdline({ "/", "?" }, {
     entries = { name = "wildmenu", separator = "|" },
   },
 })
+
+cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, {
+    { name = "cmdline" },
+  }),
+  matching = { disallow_symbol_nonprefix_matching = false },
+})
