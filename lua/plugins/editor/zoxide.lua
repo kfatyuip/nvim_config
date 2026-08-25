57a9bcb92f9600f238154e371bc3688c53e69281
Kieran Moy <kfatyuip@gmail.com>
2026-08-26 00:28:15 +0800

    refactor(zoxide): replace nvim-zoxide with nanotee/zoxide.vim

--- lua/plugins/editor/zoxide.lua
+++ lua/plugins/editor/zoxide.lua
@@ -1,9 +1,3 @@
 return {
-  "alfaix/nvim-zoxide",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  cmd = { "Z", "Zt", "Zw" },
-  opts = {
-    define_commands = true,
-    path = "zoxide",
-  },
+  "nanotee/zoxide.vim",
 }
